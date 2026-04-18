6d7e7f9a56ce7476a0cb2d7e4c4dd1d3f8d315d7
Zenidene El Farissi <122333341+zzeni05@users.noreply.github.com>
2026-04-18 17:37:43 -0400

    Add: validate-parentheses - submission-6

---
+++ Data Structures & Algorithms/validate-parentheses/submission-6.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        bracketMatching = {'{':'}', '[':']', '(': ')'}
+
+        for c in s:
+            if c in bracketMatching:
+                stack.append(c)
+            else:
+                if not stack:
+                    return False
+                top = stack.pop() 
+                if bracketMatching[top] != c:
+                    return False
+        return len(stack) == 0
+        return True
